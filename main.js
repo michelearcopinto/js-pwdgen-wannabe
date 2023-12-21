4f81100bc9da18748a83cd8119d0ac70209e8466
michelearcopinto <guestphantom1@gmail.com>
2023-12-21 16:08:44 +0100

    FIles and structure

---
+++ main.js
@@ -0,0 +1,7 @@
+let nome = prompt("Inserisci il tuo nome");
+
+let cognome = prompt("Inserisci il tuo cognome");
+
+let colore = prompt("Inserisci il tuo colore preferito");
+
+document.writeln(nome + cognome + colore + 23)
